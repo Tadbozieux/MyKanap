3a4758e246f5e92886b8636b2d0ac6de7e38d2cf
Florentin <florentin93600@gmail.com>
2022-12-16 14:54:49 +0100

    ajout des choix couleur selon id

--- js/product.js
+++ js/product.js
@@ -30,7 +30,7 @@ function addInformations(kanapé){
     const price = kanapé.price
     const altTxt= kanapé.altTxt
     const colors = kanapé.colors
-
+    const description = kanapé.description
 
 
 let image = document.createElement("img")
@@ -41,12 +41,34 @@ parentImg.appendChild (image)
 
 
 nomKanapé = document.querySelector ("#title .h1")
-title.textContent = kanapé.name
+title.textContent = name
 
 
 prixKanapé = document.getElementById ("price")
-prixKanapé.textContent = kanapé.price
+prixKanapé.textContent = price
 
+descriptionKanapé = document.getElementById ("description")
+descriptionKanapé.textContent = description
+
+const select = document.getElementById ("colors")
+
+colors.forEach((color) => {                                 //Pour chaque coueleur selon ID
+    const option = document.createElement("option")         //créer une option
+    option.value = color                                    // dont les choix sonrt les couleurs renseignées
+    option.textContent = color
+    console.log(color)
+    select.appendChild(option)
+});
+
+// let optionValue1 = document.createElement("option")              // Methode static non utilisable
+// valueKanapé.appendChild (optionValue1)
+// let optionValue2 = document.createElement("option")
+// valueKanapé.appendChild (optionValue2)
+// let optionValue3 = document.createElement("option")
+// valueKanapé.appendChild (optionValue3)
+// optionValue1.textContent = "Blue"
+// optionValue2.textContent = "White"
+// optionValue3.textContent = "Black"
 }
 
   
