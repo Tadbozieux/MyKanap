bcae2e1b6dcc5fbc8cce610a81c87a06a9dcdd9d
Florentin <florentin93600@gmail.com>
2022-12-30 16:49:05 +0100

    maj

--- js/cart.js
+++ js/cart.js
@@ -1,22 +1,26 @@
-const itemsPanier = localStorage.length
-const cart = []
-let quantityProductPanier = 0;
+const cart = [];
 
-let selectSupprimer = 0
+retrieveItemsFromCache(); //appel fonction retrieveItemsFromCache qui permet de recupérer le local storage
 
-
-for (let i = 0 ; i < itemsPanier; i++){
-    let item= localStorage.getItem(localStorage.key(i))
+function retrieveItemsFromCache() {
+  const numberOfItems = localStorage.length;
+  for (let i = 0; i < numberOfItems; i++) {
+    let item = localStorage.getItem(localStorage.key(i));
     // console.log("objet a la position " , i, "est", item) //donnes les items de JSON
-    const objetitem = JSON.parse(item)   //transforme JSON en Objet  (inverse JSON.stringify)
+    const objetitem = JSON.parse(item); //transforme JSON en Objet  (inverse JSON.stringify)
     // console.log(objetitem)
-    cart.push(objetitem)   
+    cart.push(objetitem);
     // console.log(cart)
-       
   }
-    cart.map (canap => {            // incorporation du HTML
-        document.querySelector('#cart__items').innerHTML += `
-        <article class="cart__item" data-id="${canap.id}" data-color="${canap.color}">
+}
+
+hydrateCart(cart);
+function hydrateCart(cart) {
+  cart.map((canap) => {
+    // incorporation du HTML
+
+    document.querySelector("#cart__items").innerHTML += `
+        <article class="cart__item" data-id="${canap.id}" data-color="${canap.color} data-quantity="${canap.quantity}" data-price="${canap.price}">
         <div class="cart__item__img">
           <img src="${canap.imageUrl}" alt="${canap.altTxt}">
         </div>
@@ -32,143 +36,36 @@ for (let i = 0 ; i < itemsPanier; i++){
               <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${canap.quantity}">
             </div>
             <div class="cart__item__content__settings__delete">
-              <p class="deleteItem">Supprimer</p>
+            <p class="deleteItem" data-id="${canap.id}" data-color="${canap.color}">Supprimer</p>
             </div>
           </div>
         </div>
       </article>      
             `
-           
-            
-            
-          })
-          
-          
-          const totalQuantity = document.querySelector("#totalQuantity")        //definition quantitié totale panier
-          const total2 = cart.reduce((total2, canap) => total2 + canap.quantity, 0)
-          totalQuantity.textContent = total2
-            
+  });
+  makeItemTotal();
+}
 
-          const totalPrice = document.querySelector("#totalPrice")              //definition prix totale panier
-          const total = cart.reduce((total, canap) => total + canap.price * canap.quantity, 0)
-          totalPrice.textContent = total
-          
-          
-          let deleteItem = document.querySelectorAll(".deleteItem");
-          console.log(deleteItem)
-          console.log(deleteItem.length)
-          console.log(total2)
-          console.log(canap.id)
-          
+function makeItemTotal() {
+  //definition quantitié totale panier
+  const totalQuantity = document.querySelector("#totalQuantity"); 
+  const total2 = cart.reduce((total2, canap) => total2 + canap.quantity, 0);
+  totalQuantity.textContent = total2;
 
-         
+  //definition prix totale panier
+  const totalPrice = document.querySelector("#totalPrice"); 
+  const total = cart.reduce((total, canap) => total + canap.price * canap.quantity,0  );
+  totalPrice.textContent = total;
 
+ //Reduce permet de passer un tableau [array] en 1 seulme valeur
+ //par exemple pour totalPrice il remplace un:
 
-        
-        //   function deleteProduct() {
-        //     selectSupprimer = document.querySelectorAll(".deleteItem");
-        //     console.log (selectSupprimer)
-        //     selectSupprimer.forEach((selectSupprimer) => {
-        //             selectSupprimer.addEventListener("click" , (event) => {
-        //                 event.preventDefault();
-                                    
-        //                 // On pointe le parent hiérarchique <article> du lien "supprimer"
-        //                 let myArticle = selectSupprimer.closest('article');
-        //                 console.log(myArticle);
-        //                 // on filtre les éléments du localStorage pour ne garder que ceux qui sont différents de l'élément qu'on supprime
-        //                 productRegisterInLocalStorage = productRegisterInLocalStorage.filter
-        //                 ( element => element.idProduct !== myArticle.dataset.id || element.colorProduct !== myArticle.dataset.color );
-                        
-        //                 // On met à jour le localStorage
-        //                 localStorage.setItem("produit", JSON.stringify(productRegisterInLocalStorage));
-                        
-        //                 //Alerte produit supprimé
-        //                 alert("Ce produit va être supprimé du panier.");
-                         
-                        
-        //                 // On supprime physiquement la balise <article> du produit que l'on supprime depuis son parent, si elle existe
-        //                 if (myArticle.parentNode) {
-        //                     myArticle.parentNode.removeChild(myArticle);
-        //                 }
-        //                 //-----Si, du coup, le panier est vide (le localStorage est vide ou le tableau qu'il contient est vide),...
-        //                 //...on affiche "Le panier est vide"-------------------------------------------------------------------
-        //                 if(productRegisterInLocalStorage === null || productRegisterInLocalStorage.length === 0){
-        //                     messagePanierVide();
-        //                 }
-        //                 else{
-        //                 // Et, on recalcule la quantité et le prix total du panier
-        //                 recalculTotalQuantity();
-        //                 recalculTotalPrice();
-        //                 }
-        //             }); 
-        //     })
-        // }
+ //canap.forEach((canap)) => {
+ // const totalPrixUnitaire = canap.price *canap.quantity
+ //total += TotalPrixUnitaire
+ //}
+}
 
 
-
-          // const bouton_supp = document.querySelectorAll(".deleteItem");   // On récupère l'élément sur lequel on veut détecter le clic
-          // bouton_supp.addEventListener('click', function() {          // On écoute l'événement click
-          //   bouton_supp.innerHTML = "C'est cliqué !";               // On change le contenu de notre élément pour afficher "C'est cliqué !"
-          // });
-
-        
-
-
-
-
-
-
-
-
-
-
-
-          
-        //   function deletePurchase() {
-        //     // Déclaration + Pointage de tous les éléments ".cart__item .deleteItem"
-        //     const deletePurchase = document.querySelectorAll(".cart__item .deleteItem")
-            
-        //     // Pour chaque éléments [...]
-        //     deletePurchase.forEach((purchase) => {
-        //         // Écoute du click sur bouton "Supprimer" de chaque produit
-        //         purchase.addEventListener("click", () => {
-        //             // Appel du Panier en Local Storage
-        //             // let myCart = JSON.parse(localStorage.getItem("Cart"))
-        //             // Boucle : Pour chaque élément du Panier [...]
-        //             for (let a = 0, b = cart.length; a < b; a++)
-        //                 if (
-        //                     // Si correspondance Panier/dataset (id/color)
-        //                     cart[a].id === canap.id &&
-        //                     cart[a].color === canap.color
-        //                 ) {
-        //                     // Variable utile pour suppression
-        //                     const picked = [a];
-        //                     // Suppression de 1 élément à l'indice picked
-        //                     cart.splice(picked, 1)
-        //                     // Renvoi du (nouveau) panier converti dans le Local Storage 
-        //                     localStorage.cart = JSON.stringify(cart)
-        
-        //                     // Suppression de l'Affichage HTML
-        //                     const displayToDelete = document.querySelector(
-        //                         `article[data-id="${canap.id}"][data-color="${canap.color}"]`)
-        //                     displayToDelete.remove()
-        
-        //                     // Confirmation(s) de la console
-        //                     console.log("Article supprimé")
-        //                     console.log("Panier mis à jour :", cart)
-        
-        //                     // Si Panier vide
-        //                     if (cart && cart.length == 0) {
-        //                         // Vider Local Storage ([] vide)                       
-        //                         window.localStorage.clear()
-        //                         // Affichage informatif
-        //                         document.querySelector("#totalQuantity").innerHTML = "0"
-        //                         document.querySelector("#totalPrice").innerHTML = "0"
-        //                         document.querySelector("h1").innerHTML =
-        //                             "Vous n'avez pas d'article dans votre panier"
-        //                     }
-        //                     totalCart(); // Appel de la fonction Total Quantité/Prix
-        //                 }
-        //         })
-        //     })
-        // }
+let test = document.querySelector(".itemQuantity")
+console.log(test)
